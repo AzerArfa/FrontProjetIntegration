f71af2fbfdaabe88e840dbbd82b9187a8022df42
unknown <azerarfa606606@gmail.com>
2024-01-11 19:17:53 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AjoutProduitComponent } from './ajout-produit/ajout-produit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"accueil",component:AccueilComponent},
+  {path:"navbar",component:NavbarComponent},
+  {path:"login",component:LoginComponent},
+  {path:"ajout",component:AjoutProduitComponent},
+  {path:"register",component:RegisterComponent},
+  { path: "", redirectTo: "accueil", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AjoutProduitComponent } from './ajout-produit/ajout-produit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AccueilComponent,
+    LoginComponent,
+    RegisterComponent,
+    AjoutProduitComponent
   ],
   imports: [
     BrowserModule,
